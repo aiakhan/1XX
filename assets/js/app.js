a8c2071a1bec89a00efac1f582d3009deda6eb78
aiakhan <iram.khan95@gmail.com>
2018-03-05 00:49:38 -0500

    moved init function to new app.js and add ajaz call to begin the building the menu

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+
+function init() {
+
+
+
+     window.addEventListener('scroll', function (e) {
+
+          var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+
+               shrinkOn = 300,
+
+               header = document.querySelector("header");
+
+          if (distanceY > shrinkOn) {
+
+               classie.add(header, "smaller");
+
+          } else {
+
+               if (classie.has(header, "smaller")) {
+
+                    classie.remove(header, "smaller");
+
+               }
+
+          }
+
+     });
+
+
+
+     $.ajax({
+
+          method: 'GET',
+
+          url: 'assets/data/menu.json',
+
+          dataType: 'json',
+
+          success: function (data) {
+			  console.log('all good');
+			  console.log(data.menu.leading);
+			  console.log(data.men);
+			  if (data.men.length> 0){
+				  
+				  data.menu.forEach(function(dat){
+					 console.log(data.MenuName);
+					 console.log(data.MenuLink);
+					  
+					 $('nav').append('<a href="' + data.MenuLink +'">' + data.MenuName+'</a>');
+			  });
+				  
+				  
+			  }
+			  
+									
+									
+		  },
+		 
+
+ 
+          error: function () {
+
+               console.log('all is not good');
+
+          }
+
+     });
+
+
+
+}
+window.onload = init();
+
+
+
+
+
+
